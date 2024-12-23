bfea1c96b0825ec1e2215c5f966cc2bec975c995
Vitoria-Maria0912 <vitoria.nascimento@ccc.ufcg.edu.br>
2024-12-23 14:19:27 -0300

    feat: Creating database tables for Administrators.

--- backend/src/express/server.ts
+++ backend/src/express/server.ts
@@ -7,10 +7,6 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
-const server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+const server = app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 
-export const closeServer = () => {
-    server.close();
-};
+export const closeServer = () => { server.close(); };
