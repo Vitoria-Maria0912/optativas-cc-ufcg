33728baa8f05a40ad89df8f5e8bd2a5c9021f068
Vitoria-Maria0912 <vitoria.nascimento@ccc.ufcg.edu.br>
2024-12-28 23:27:27 -0300

    build: Setting login route.

--- backend/src/express/server.ts
+++ backend/src/express/server.ts
@@ -3,8 +3,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { UserController } from '../controller/UserController';
+import { AuthController } from '../controller/AuthController';
 
-export const app = express();
+const app = express();
+
 dotenv.config();
 app.use(cors());
 app.use(express.json());
@@ -14,11 +16,13 @@ const PORT = process.env.PORT || 3001;
 export const JWT_SECRET = process.env.JWT_SECRET || 'default-secret';
 
 const userController = new UserController();
+const authController = new AuthController();
 
 const server = app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 
 export const closeServer = () => { server.close(); };
 
 app.post('/users', (req, res) => {userController.createUser(req,res)});
+app.post('auth/login', (req, res) => {authController.createLogin(req,res)});
 
 export default app;
