db7b8275b2b6ca820d9a4492a6b1c59dc4a7da70
Vitória Maria do Nascimento <116987726+Vitoria-Maria0912@users.noreply.github.com>
2025-02-06 18:15:46 +0000

    feat: Create getAllDisciplines() at controller.

--- backend/src/controller/DisciplineController.ts
+++ backend/src/controller/DisciplineController.ts
@@ -34,4 +34,18 @@ export class DisciplineController {
         }
         return response.status(codeResponse).json(responseBody)
     }
+
+    async getAllDisciplines(request: Request, response: Response): Promise<Response>  {
+        var codeResponse: number;
+        var responseBody: object;
+        try {
+            const disciplines = await this.disciplineService.getAllDisciplines();
+            responseBody = { message: "Disciplines were found successfully!", disciplines};
+            codeResponse = 200;
+        } catch (error: any) {
+            responseBody = { message: (!error.message) ? "Error trying to get all disciplines!" : error.message};
+            codeResponse = error.statusCode && !isNaN(error.statusCode) ? error.statusCode : 400;
+        }
+        return response.status(codeResponse).json(responseBody)
+    }
 }
