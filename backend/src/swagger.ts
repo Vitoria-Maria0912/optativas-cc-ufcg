c230c6c2d7fa247bf9c34f93de1f538deb011fad
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2025-02-19 22:35:15 -0300

    refactor: ajusting swagger to user separated yaml file

--- backend/src/swagger.ts
+++ backend/src/swagger.ts
@@ -1,6 +1,8 @@
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import { Express } from "express";
+import YAML from 'yamljs';
+import path from "path";
 
 const options: swaggerJsdoc.Options = {
   definition: {
@@ -23,5 +25,7 @@ const options: swaggerJsdoc.Options = {
 const swaggerSpec = swaggerJsdoc(options);
 
 export function setupSwagger(app: Express) {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  // app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  const swaggerDocument = YAML.load(path.resolve('src/swagger/swagger.yml'));
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 }
